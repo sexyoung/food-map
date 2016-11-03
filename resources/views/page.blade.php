9f78ef9c9c61dbe5251b768778d59200575394f9
寫詩羊 <Habuche@gmail.com>
2016-11-03 22:44:02 +0800

    還是把內頁的影片拿掉吧

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -13,10 +13,7 @@
     "list" => $departments
   ])
   <header class="{{$college}}">
-    <video class="bg" width="640" height="360" loop muted autoplay poster="{{url("/images/".array_rand($departments).".jpg")}}">
-      <source src="/video/video.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+    <div class="bg" style="background-image: url({{url("/images/".array_rand($departments).".jpg")}})"></div>
     <div class="container">
       <div class="row">
         <div class="col-sm-12 text-center">
