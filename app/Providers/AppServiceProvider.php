e952ad0841c562185442fa07e1c616be0fa396a9
寫詩羊 <Habuche@gmail.com>
2016-12-12 00:12:24 +0800

    定義所有學院分類變數，並建立 PageAlbum Model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->share('foods', [
+          'rice' => ["lu-meat-rice", "oil-rice", "meat-rice", "turkey-meat-rice"],
+          'tofu' => ["stinky-tofu","preserved-egg-tofu","mapo-tofu"],
+          'chicken' => ["salty-crisp-chicken","chicken-row","chicken-ass","chicken-wings","chicken-feet"],
+          'vermicelli' => ["clear-vermicelli","large-intestine-vermicelli","oysters-vermicelli","sesame-oil-vermicelli"],
+          'oysters' => ["oyster-fried-oyster","oyster-soup","oyster-fritter","baked-oysters"],
+          'beef_noodles' => ["braised-beef-noodles","stew-beef-noodles","tamato-beef-noodles"],
+          'thick_soup' => ["fried-spanish-mackerel-thick-soup","squid-thick-soup","calamary-thick-soup","pork-thick-soup"],
+          'staple' => ["pineapple-cakes","ci-cake","sun-cake","cow-tongue-cake","peanut-candy"],
+          'pearl' => ["black-tapioca-pearls-curd","heart-tapioca","pearl-milk-tea","three-color-tapioca"]
+        ]);
     }
 
     /**
---
+++ database/migrations/2016_12_11_142928_create_page_albums_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePageAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type', 50);
+            $table->string('page_id', 100);
+            $table->string('page_name', 100);
+            $table->string('page_user', 100);
+            $table->string('page_email', 100);
+            $table->string('album_id', 100);
+            $table->string('location', 100);
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->boolean('confirmed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('page_albums');
+    }
+}
